c75ad2334f5294abdd6b0e928453283f1df90090
thiagotandrade <thiagotales@gmail.com>
2020-09-21 20:44:16 -0300

    Configurando Rotas e animando os forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import AppProvider from './hooks';
 
 import GlobalStyle from './styles/global';
 
+import Routes from './routes';
+
 const App: React.FC = () => (
-  <>
+  <Router>
     <AppProvider>
-      <SignIn />
+      <Routes />
     </AppProvider>
 
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
