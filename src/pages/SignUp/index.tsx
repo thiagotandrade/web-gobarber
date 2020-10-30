66dc940094a0ed7a5b4ce702ea60e8694418fcc0
thiagotandrade <thiagotales@gmail.com>
2020-10-30 09:54:13 -0300

    finish update user profile

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -46,7 +46,7 @@ const SignUp: React.FC = () => {
           abortEarly: false,
         });
 
-        api.post('/users', data);
+        await api.post('/users', data);
 
         history.push('/');
 
